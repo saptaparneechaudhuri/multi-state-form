2a2ad7d9669c5fece838c7ea379e395b1b758f92
Saptaparnee Chaudhuri <saptaparnee8@gmail.com>
2023-07-10 13:18:19 +0530

    adding a .env file

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBu0aiwXfyS5G41TMsLdKHOiED0BKCihR4",
-  authDomain: "multi-step-form-db.firebaseapp.com",
-  projectId: "multi-step-form-db",
-  storageBucket: "multi-step-form-db.appspot.com",
-  messagingSenderId: "97215121338",
-  appId: "1:97215121338:web:e8c6f9ef916e4efc497f83",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
